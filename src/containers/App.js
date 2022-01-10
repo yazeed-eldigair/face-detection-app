0673990856c507a8206ad04df101b9fcd102feb4
YazeedEld <yazeed_eldigair@yahoo.com>
2022-01-10 22:24:16 +0400

    changing fetch urls

--- src/containers/App.js
+++ src/containers/App.js
@@ -62,7 +62,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({ imageURL: this.state.input });
-      fetch("http://localhost:3001/imageurl", {
+      fetch("https://secret-headland-04901.herokuapp.com/imageurl", {
         method: "post",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -71,7 +71,7 @@ class App extends Component {
       }).then(response => response.json())
       .then((response) => {
         if (response) {
-          fetch("http://localhost:3001/image", {
+          fetch("https://secret-headland-04901.herokuapp.com/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
